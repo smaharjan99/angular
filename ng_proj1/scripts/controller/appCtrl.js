9ed914c0243d59cf32d890f0007ab7aca9bab44b
Sunil Maharjan <smaharjan237@gmail.com>
2016-10-27 12:28:48 -0500

    angular commenced

---
+++ ng_proj1/scripts/controller/appCtrl.js
@@ -0,0 +1,9 @@
+'use strict!';
+
+angular.module("appName")
+    .controller("appCtrl",["$scope", function($scope){
+        $scope.header="hello World";
+        $scope.test=function(){
+            $scope.header="oh look it changed";
+        }
+    }]);
